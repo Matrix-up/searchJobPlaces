6785c6b1ec1e2f95e06e99ec173f0631e043ce83
martin <mgladis92@gmail.com>
2020-11-07 22:47:42 +0100

    End the pracuj.pl service

--- searchJobPlaces/views.py
+++ searchJobPlaces/views.py
@@ -9,33 +9,45 @@ def index(request):
             'search': request.POST.get('search'),
             'place': request.POST.get('place'),
         }
-        url = "https://www.pracuj.pl/praca/" + context['search'] + ";kw/" + context['place'] + ';wp'
-        source = Page(url)
-        soup = BeautifulSoup(source.html, 'html.parser')
-
-        results = soup.find('div', class_="results")
-        positions = results.find_all("li", class_="results__list-container-item")
 
         names = []
         links = []
         employers = []
         locations = []
-        contracts_type = []
-        for position in positions:
-            name = position.find('a', class_="offer-details__title-link")
-            location = position.find('li', class_="offer-labels__item offer-labels__item--location")
-            employer = position.find('a', class_="offer-company__name")
-            contract_type = position.find('li', {'data-test': 'list-item-offer-type-of-contract'})
-            if name is not None:
-                names.append(name.find(text=True))
-                links.append(name['href'])
-                employers.append(employer.find(text=True))
-                locations.append(location.find(text=True))
-                if contract_type is None:
-                    contracts_type.append("<i class='red-text'>Brak danych</i>")
+
+        page = 1
+        positions = 1  # Value 1 is set for entrance to loop
+
+        while positions:
+            url = "https://www.pracuj.pl/praca/" + context['search'] + ";kw/" + context['place'] + ';wp?rd=0&pn=' + \
+                  str(page)
+            source = Page(url)
+            soup = BeautifulSoup(source.html, 'html.parser')
+
+            results = soup.find('div', class_="results")
+            positions = results.find_all("li", class_="results__list-container-item")
+            for position in positions:
+                name = position.find('a', class_="offer-details__title-link")
+                location = position.find('li', class_="offer-labels__item offer-labels__item--location")
+                employer = position.find('a', class_="offer-company__name")
+
+                if name is None:
+                    name = position.find('button', class_="offer-details__title-link")
+                    if name is None:
+                        continue
+                    location = position.findAll('a', class_="offer-regions__label")
+                    for loc in location:
+                        names.append(name.find(text=True))
+                        links.append(loc['href'])
+                        locations.append(loc.find(text=True))
+                        employers.append(employer.find(text=True))
                 else:
-                    contracts_type.append(contract_type.find(text=True))
-        context['jobsList'] = zip(names, links, employers, locations, contracts_type)
+                    names.append(name.find(text=True))
+                    links.append(name['href'])
+                    locations.append(location.find(text=True))
+                    employers.append(employer.find(text=True))
+            page += 1
+        context['jobsList'] = zip(names, links, employers, locations)
         return render(request, 'result.html', context)
     else:
         return render(request, 'index.html')
