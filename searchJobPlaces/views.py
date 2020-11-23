eb541062f6a0b9fa6eb664785800da48e25f5956
martin <mgladis92@gmail.com>
2020-11-23 15:57:03 +0100

    add infopraca.pl and repair praca.pl

--- searchJobPlaces/views.py
+++ searchJobPlaces/views.py
@@ -14,6 +14,7 @@ def index(request):
 
     context = pracuj_pl(context)
     context = praca_pl(context)
+    context = infoPraca_pl(context)
 
     return render(request, 'result.html', context)
 
@@ -68,14 +69,15 @@ def praca_pl(context):
     locations = []
     first_site = ""
 
+    search = context['search'].replace(", ", ",").replace(" ", ",")
+    place = removePolishLetters(context['place'])
+
     page = 1
     while True:
         if page == 1:
-            url = 'https://www.praca.pl/s-' + context['search'].replace(", ", ",").replace(" ", ",") + ',' + \
-                  context['place'] + '.html'
+            url = 'https://www.praca.pl/s-' + search + ',' + place + '.html'
         else:
-            url = 'https://www.praca.pl/s-' + context['search'].replace(", ", ",").replace(" ", ",") + ',' + \
-                  context['place'] + '_' + str(page) + '.html'
+            url = 'https://www.praca.pl/s-' + search + ',' + place + '_' + str(page) + '.html'
         source = Page(url)
         soup = BeautifulSoup(source.html, 'html.parser')
         offers = soup.find('div', class_="listing--with-blocks")
@@ -110,4 +112,56 @@ def praca_pl(context):
 
     context['praca_pl'] = list(zip(names, links, employers, locations))
 
+    return context
+
+
+def removePolishLetters(text):
+    letters = 'ąćęłńóśźż'
+    ascii_replacements = 'acelnoszz'
+
+    translator = str.maketrans(letters, ascii_replacements)
+
+    return text.translate(translator)
+
+
+def infoPraca_pl(context):
+    names = []
+    links = []
+    employers = []
+    locations = []
+
+    page = 1
+
+    positions = True  # Value True is set for entrance to loop
+
+    while positions:
+        url = "https://www.infopraca.pl/praca?q=" + context['search'] + '&d=0&pg=' + str(page)
+        if context['place'] is not '':
+            url += "&lc=" + context['place']
+        source = Page(url)
+        soup = BeautifulSoup(source.html, 'html.parser')
+
+        positions = soup.find_all("div", class_="position-head container")
+        for position in positions:
+            name = position.find('a', class_="job-offer")
+            location = position.find('h4', class_="serp-one-location")
+            employer = position.find('h3', class_="p-name company")
+
+            name_text = name['title'].rsplit(' ', 1)[0].split(' ', 1)[1]
+
+            if location is None:
+                location = position.findAll('a', class_="mr-loc")
+
+                for loc in location:
+                    names.append(name_text)
+                    links.append(loc['href'])
+                    locations.append(loc.find(text=True))
+                    employers.append(employer.find(text=True))
+            else:
+                names.append(name_text)
+                links.append(name['href'])
+                locations.append(location.find(text=True))
+                employers.append(employer.find(text=True))
+        page += 1
+    context['infoPraca_pl'] = list(zip(names, links, employers, locations))
     return context
